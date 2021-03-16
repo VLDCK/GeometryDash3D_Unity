5cdc9009724cfcc2a543479d482aa52ed658f4f7
vldick <vldick@users.noreply.github.com>
2021-03-16 12:09:27 +0200

    Add project to git
    First commit to git
    Menu + gravity

---
+++ Geometry Dash 3D/Assets/Scripts/MenuController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Основний скрипт для меню гри
+///     Має функції запуску системи вибору рівнів
+///     +Функції виклику меню налаштування 
+///     +Функція виходу з гри 
+///     Функція перегляду статистики за ігровий період
+/// </summary>
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject MainMenuButtons;
+    public GameObject SettingMenuButtons;
+
+    public void StartGame()
+    {
+      SceneManager.LoadScene("SimpleScene");
+    }
+
+    //Виклик меню налаштування
+    public void CallingSettingMenu() 
+    {
+        MainMenuButtons.SetActive(false);
+        SettingMenuButtons.SetActive(true);
+    }
+
+    //Виклик основного меню 
+    public void CallingMainMenu()
+    {
+        MainMenuButtons.SetActive(true);
+        SettingMenuButtons.SetActive(false);
+    }
+
+    //Вихід з гри
+    public void Exit() 
+    {
+        Application.Quit(); // Завершення виконання програми
+    }
+
+
+}
---
+++ Geometry Dash 3D/Assets/Scripts/ссс.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ссс : MonoBehaviour
+{
+    CubeMechanics cubeMechanics;
+    public GameObject cubeBoxCollider;
+    public float rotationPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeBoxCollider = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!CubeMechanics.cubeContoller.isGrounded)
+        {
+
+            Quaternion rotX = Quaternion.AngleAxis(rotationPoint, Vector3.right);
+            transform.rotation *= rotX;
+            //if(transform.Rotate(90,0,0))
+        }  
+    }
+}
